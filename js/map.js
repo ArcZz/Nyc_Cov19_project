b0a121216ef67c653a53e2fd59980650e23e2357
Zhang Zhi <Zhang48240@gmail.com>
2020-05-13 16:00:27 -0400

    push

--- js/map.js
+++ js/map.js
@@ -1,10 +1,5 @@
-
-
 var width = 960,
   height = 960;
-var draw = 0;
-
-
 
 var projection = d3.geoMercator()
   .scale(81000)
@@ -14,13 +9,10 @@ var svg = d3.select("#d3map")
   .append("svg")
   .attr("viewBox", [0, 0, 960, 960]);
 
-
-
-d3.queue()
-  .defer(d3.json, "data/nyctop.json")
-  .defer(d3.csv, "data/zdata.csv")
-  .await(makeMap);
-
+var anima = d3.select("#anima")
+  .append("svg")
+  .attr("viewBox", [0, 0, 960, 960]);
+  
 
 function toPercent(point) {
   var str = Number(point * 100).toFixed(2);
@@ -28,20 +20,26 @@ function toPercent(point) {
   return str;
 }
 
-function makeMap(error, nyctopo, zdata) {
 
+
+
+d3.queue()
+  .defer(d3.json, "data/nyctop.json")
+  .defer(d3.csv, "data/zdata.csv")
+  .defer(d3.csv, "https://raw.githubusercontent.com/thecityny/covid-19-nyc-data/master/zcta.csv")
+  .await(makeMap);
+
+
+function makeMap(error, nyctopo, zdata,zcta) {
   if (error) throw error;
   // console.log(data.objects.zipcode.geometries.)
-
   var num = {}; // Create empty object for holding dataset
   var posi = [];
   zdata.forEach(function (d) {
-
     num[d.MODZCTA] = +parseInt(d.Positive);
     posi.push(parseInt(d.Positive))
 
   });
-
 
   var color = d3.scaleSequential(d3.interpolateYlOrRd)
     .domain(d3.extent(posi))
@@ -60,17 +58,16 @@ function makeMap(error, nyctopo, zdata) {
     .attr("d", d3.geoPath().projection(projection))
     .attr("stroke", "#747473")
     .attr("stroke-width", .8)
-
     .attr("fill", function (d) {
-      var zipcode = d.properties.zipcode;
 
+   
+      var zipcode = d.properties.zipcode;
       if (typeof (num[zipcode]) == "undefined") {
         return "Lavender";
       }
 
       return color(num[zipcode]);
     })
-
     .on("mouseover", function (d, i) {
 
 
@@ -99,7 +96,6 @@ function makeMap(error, nyctopo, zdata) {
 
 
     })
-
     .on("mouseout", function (d, i) {
       var zipcode = d.properties.zipcode;
       if (typeof (num[zipcode]) == "undefined") {
@@ -108,63 +104,111 @@ function makeMap(error, nyctopo, zdata) {
       d3.select(this).attr("fill", color(num[zipcode]));
     });
 
+ 
+ var newArr = zcta.filter(function(p){
+  return p.timestamp === "2020-05-12T04:00:00Z";
+});
+
+//   var ba = newArr.filter(function(p){
+//         return p.zcta === "10001";
+//         });
+// console.log(ba)
+
+  anima.selectAll("path")
+    .data(nyc.features)
+    .enter().append("path")
+    .attr("d", d3.geoPath().projection(projection))
+    .attr("stroke", "#747473")
+    .attr("stroke-width", 1)
+    .attr("fill", function (d) {
+      
+      var zipcode = d.properties.zipcode;
+      var ba = newArr.filter(function(p){
+        return p.zcta === zipcode.toString()  ;
+        });
+      
+      if ((ba.length === 0) || (typeof (ba[0].positive) == "undefined")) {
+        return "white";
+      }
+      if ( (ba[0].positive) == 0) {
+        return "white";
+      }
+       return color(ba[0].positive);
+    })
+    var date = makeMapDate();
+    console.log(date)
 
 };
 
-// var odometer = new Odometer({
-//   el: $('.odometer')[0],
-//   value: 0,
-//   theme: 'minimal',
-//   duration: 1000
-// });
-// odometer.render();
+function makeMapDate(a, b){
+  var date = a + " "+ b;
+  return date;
+}
+$( function() {
+    $( "#timeslider" ).slider({
+      range: "min",
+      value: new Date('2020.03.31').getTime() / 1000,
+       step: 86400,
+      min: new Date('2020.03.31').getTime() / 1000,
+      max: new Date('2020.05.10').getTime() / 1000,
+      slide: function( event, ui ) {
+        $( "#amount" ).val( new Date(ui.value *1000).toDateString()   );
+        let time =  new Date(ui.value *1000) 
+         let date = time.getDate()
+         let month = time.getMonth()
+       makeMapDate(date,month);
+      }
+    });
+
+    $( "#amount" ).val(new Date($( "#timeslider" ).slider( "value" )*1000).toDateString() );
+  
+  } );
+  
+    
+
 
 
 $(document).ready(function () {
-
-
-// setTimeout(function(){
-//     $("#o1").html("184319");
-//      $("#o2").html("15101");
-//       $("#o3").html("48939");
-    
-// }, 2000);
+        
 
   $(".dropdown-item").click(function () {
     var val = $(this).attr("id");
     switch (val) {
       case "dpd2":
-  
-        $("#showingmap").attr('src',"nycrate.html"); 
+
+        $("#showingmap").attr('src', "nycrate.html");
         break;
       case "dpd3":
-        $("#showingmap").attr('src',"zctab.html"); 
+        $("#showingmap").attr('src', "zctab.html");
         break;
       case "dpd4":
-        $("#showingmap").attr('src',"zctap.html"); 
+        $("#showingmap").attr('src', "zctap.html");
         break;
       default:
-         $("#showingmap").attr('src',"nycnum.html");
+        $("#showingmap").attr('src', "nycnum.html");
     }
-});
- $(".borobutton").click(function () {
+  });
+
+
+
+  $(".borobutton").click(function () {
     var val = $(this).attr("id");
     switch (val) {
       case "img1":
-        console.log(val)
-        $("#showimag").attr('src',"picture/part1/bronx_data.png"); 
+      
+        $("#showimag").attr('src', "picture/part1/bronx_data.png");
         break;
       case "img2":
-        $("#showimag").attr('src',"picture/part1/borrklyn_data.png"); 
+        $("#showimag").attr('src', "picture/part1/borrklyn_data.png");
         break;
       case "img3":
-        $("#showimag").attr('src',"picture/part1/manhattan_data.png"); 
+        $("#showimag").attr('src', "picture/part1/manhattan_data.png");
         break;
       case "img4":
-        $("#showimag").attr('src',"picture/part1/queens_data.png"); 
+        $("#showimag").attr('src', "picture/part1/queens_data.png");
         break;
       default:
-         $("#showimag").attr('src',"picture/part1/Staten_Island_data.png");
+        $("#showimag").attr('src', "picture/part1/Staten_Island_data.png");
     }
 
   });
@@ -181,32 +225,32 @@ function check_if_in_view() {
   var window_top_position = $window.scrollTop();
   var window_bottom_position = (window_top_position + window_height);
 
-  $.each($animation_elements, function() {
+  $.each($animation_elements, function () {
     var $element = $(this);
     var element_height = $element.outerHeight();
     var element_top_position = $element.offset().top;
     var element_bottom_position = (element_top_position + element_height);
-    
+
     //check to see if this current container is within viewport
     if ((element_bottom_position >= window_top_position) &&
       (element_top_position <= window_bottom_position)) {
-       setTimeout(function(){
+      setTimeout(function () {
         $("#o1").html("184319");
         $("#o2").html("15101");
         $("#o3").html("48939");
-             flag = 1;
-        }, 1000);
+        flag = 1;
+      }, 1000);
     } else {
 
     }
   });
 }
-function number_animation(){
-  if (flag == 0){
+
+function number_animation() {
+  if (flag == 0) {
     check_if_in_view();
-  
-  }
-  else{
+
+  } else {
 
   }
 }
@@ -224,7 +268,7 @@ $window.trigger('scroll');
 //     $("#o1").html("184319");
 //      $("#o2").html("15101");
 //       $("#o3").html("48939");
-    
+
 // }, 2000);
 
 
